3be622d199055f46dfc27e2afe6c350fd6c46a4c
Ido S <ido.pluto@gmail.com>
2023-09-02 01:42:35 +0300

    fix: typing

--- src/download/stream-progress/istream-progress.ts
+++ src/download/stream-progress/istream-progress.ts
@@ -1,5 +1,5 @@
-export interface IStreamProgress {
-    init(): Promise<void>;
+export abstract class IStreamProgress {
+    abstract init(): Promise<void>;
 
-    progress(callback: (progressBytes: number, totalBytes: number) => void): Promise<any>;
+    abstract progress(callback: (progressBytes: number, totalBytes: number) => void): Promise<any>;
 }
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import pullFileCLI from "./download/index.js";
 import CLIPullProgress from "./download/cli-pull-progress.js";
 import {IStreamProgress} from "./download/stream-progress/istream-progress.js";
 import {truncateText} from "./utils/truncate-text.js";
+import CopyProgress from "./download/stream-progress/copy-progress.js";
+import FastDownload from "./download/stream-progress/fast-download.js";
 
 export {
     pullFileCLI,
     CLIPullProgress,
-    truncateText
+    truncateText,
+    CopyProgress,
+    FastDownload
 };
 
 export type {
