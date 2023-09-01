c40fb4e7313a11d143c12d44edc421f65eed344e
Ido S <ido.pluto@gmail.com>
2023-09-02 01:46:06 +0300

    fix: export type

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
 import pullFileCLI from "./download/index.js";
 import CLIPullProgress from "./download/cli-pull-progress.js";
-import {IStreamProgress} from "./download/stream-progress/istream-progress.js";
 import {truncateText} from "./utils/truncate-text.js";
 import CopyProgress from "./download/stream-progress/copy-progress.js";
 import FastDownload from "./download/stream-progress/fast-download.js";
+import {IStreamProgress} from "./download/stream-progress/istream-progress.js";
 
 export {
     pullFileCLI,
     CLIPullProgress,
     truncateText,
     CopyProgress,
-    FastDownload
-};
-
-export type {
+    FastDownload,
     IStreamProgress
 };
